4bb21c4aa579499cbf5161275ac072a71faebe66
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2024-03-24 23:32:39 +0700

    Add files via upload

---
+++ TPMOD06_1302223055/TPMOD06_1302223055/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TPMOD06_1302223055.SayaTubeVideo;
+
+namespace program;
+class Progam
+{
+
+    public static void Main(String[] Args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("jejak rosul", 0);
+        for (int i = 0; i < 250; i++)
+        {
+            video.incrementPlayCount(100000000);
+            video.printVideoDetails();
+        }
+        Console.WriteLine("max play count :" + int.MaxValue);
+
+    }
+}
---
+++ TPMOD06_1302223055/TPMOD06_1302223055/SayaTubeVideo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD06_1302223055.SayaTubeVideo
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int PlayCount;
+
+        public SayaTubeVideo(String title, int PlayCount)
+        {
+            if (title == null || title == "")
+            {
+                throw new ArgumentException("Title cannot be null or empty");
+            }
+            Random r = new Random();
+            this.id = r.Next(100000, 999999);
+            this.title = title;
+            this.PlayCount = PlayCount;
+        }
+
+        public void incrementPlayCount(int x)
+        {
+            try
+            {
+                checked
+                {
+                    if (x > 100000000)
+                    {
+                        PlayCount += 100000000;
+                    }
+                    else
+                    {
+                        PlayCount += x;
+                    }
+
+                }
+            }
+            catch (OverflowException e)
+            {
+                this.PlayCount = int.MaxValue;
+                Console.WriteLine("Play count is too high to increment \n");
+            }
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + PlayCount);
+        }
+    }
+}
